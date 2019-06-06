96611834cb470a3324c1a1e1efbe6a3bcd8978a5
pfranczak <franczak.przemyslaw3@gmail.com>
2019-06-06 22:38:18 +0200

    ellipsis usernames

--- src/components/Chat/OnlineUsers.tsx
+++ src/components/Chat/OnlineUsers.tsx
@@ -8,14 +8,26 @@ const StyledUserList = styled.div`
     border-radius: 7px 0 0 7px;
     flex: 1;
     height: 100%;
-    overflow-y: scroll;
+    overflow-x: hidden;
+    overflow-y: scroll; 
+`;
+
+const StyledItem = styled.div`
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 `;
 
 const OnlineUsers: React.FC<{usersList: Array<UserInterface>}> = ({usersList}) => {
     return (
         <StyledUserList>
             {usersList.map(({username, status}, index) => {
-                return <div key={username}>{username}{USER_STATUS[status]}</div>
+                return <StyledItem
+                    key={username}
+                    title={username}
+                >
+                    {username}{USER_STATUS[status]}
+                </StyledItem>
             })}
         </StyledUserList>
     );
